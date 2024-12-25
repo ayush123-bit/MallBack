d9c67dc4501bdd5f8bbd7fe075cfde6cd6908e06
ayush123-bit <145470522+ayush123-bit@users.noreply.github.com>
2024-12-25 17:26:04 +0530

    database fixed

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/MallDataManage")
+mongoose.connect("mongodb+srv://userayush:1234@cluster0.p7zfp.mongodb.net/MallDataManage")
   .then(() => {
     console.log("Connection Successful");
   })
